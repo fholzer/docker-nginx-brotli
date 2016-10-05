c349eaa2d570ae008ba97f70c2ed5e7b7101fc14
Ferdinand Holzer <ferdinand.holzer@gmail.com>
2016-10-06 01:18:30 +0200

    Add unit tests and travis.yml

---
+++ runTests.sh
@@ -0,0 +1,1 @@
+docker run --rm -v $(pwd)/tests.sh:/tests.sh --entrypoint sh nginx-brotli /tests.sh
---
+++ tests.sh
@@ -0,0 +1,10 @@
+if [ "x`whoami`" == "xroot" ]; then
+	echo "Installing test dependencies..."
+	apk update
+	apk add mercurial openssl ffmpeg uwsgi uwsgi-python perl perl-fcgi perl-gd perl-io-socket-ssl perl-net-ssleay
+	su -s /bin/sh -c /runTests.sh - nginx
+else
+	hg clone http://mdounin.ru/hg/nginx-tests
+	cd nginx-tests
+	TEST_NGINX_BINARY=/usr/sbin/nginx-debug TEST_NGINX_MODULES=/etc/nginx/modules TEST_NGINX_LEAVE=1 prove .
+fi
