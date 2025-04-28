d1ff600552d69cb68fb7499f3c0d70281cdad5fc
Ferdinand Holzer <ferdinand.holzer@gmail.com>
2025-04-28 21:51:11 +0200

    ci: switch from nginx test hg to git repo

--- tests.sh
+++ tests.sh
@@ -1,10 +1,10 @@
 if [ "x`whoami`" == "xroot" ]; then
 	echo "Installing test dependencies..."
 	apk update
-	apk add mercurial openssl ffmpeg uwsgi uwsgi-python perl perl-fcgi perl-gd perl-io-socket-ssl perl-net-ssleay perl-test-harness-utils
+	apk add git openssl ffmpeg uwsgi uwsgi-python perl perl-fcgi perl-gd perl-io-socket-ssl perl-net-ssleay perl-test-harness-utils
 	su -s /bin/sh -c /tests.sh - nginx
 else
-	hg clone http://hg.nginx.org/nginx-tests
+	git clone https://github.com/nginx/nginx-tests.git
 	cd nginx-tests
 	ls -1 *.t | grep -v '^http_listen.t$' | TEST_NGINX_BINARY=/usr/sbin/nginx-debug TEST_NGINX_MODULES=/etc/nginx/modules TEST_NGINX_LEAVE=1 xargs prove
 fi
